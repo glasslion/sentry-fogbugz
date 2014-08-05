8bd1108923e9dca4820db2c76341f178d1709a54
Leonardo.Z <glasslion@gmail.com>
2014-08-05 13:08:56 +0800

    Add doc for fogbugz form fields and a few PEP8 fixes

--- sentry_fogbugz/plugin.py
+++ sentry_fogbugz/plugin.py
@@ -15,18 +15,20 @@ import sentry_fogbugz
 
 class FogbugzOptionsForm(forms.Form):
     host_url = forms.URLField(
-        help_text=_("With schema and trailing slash e.g. https://fogcreek.fogbugz.com/"))
+        help_text=_("With schema and trailing slash. "
+                    "e.g. https://fogcreek.fogbugz.com/"))
     secret_token = forms.CharField(
         widget=forms.TextInput(attrs={'class': 'span9'}))
+    project = forms.CharField(
+        label="Default Case Project (Optional)",
+        required=False, widget=forms.TextInput(attrs={'class': 'span9'}))
     area = forms.CharField(
         label="Default Case Area (Optional)",
         required=False, widget=forms.TextInput(attrs={'class': 'span9'}))
     category = forms.CharField(
         label="DefaultCase Category (Optional)",
         required=False, widget=forms.TextInput(attrs={'class': 'span9'}))
-    project = forms.CharField(
-        label="Default Case Project (Optional)",
-        required=False, widget=forms.TextInput(attrs={'class': 'span9'}))
+
 
     def clean(self):
         config = self.cleaned_data
@@ -52,7 +54,8 @@ class FogbugzPlugin(IssuePlugin):
     project_conf_form = FogbugzOptionsForm
 
     def is_configured(self, project, **kwargs):
-        return all((self.get_option(k, project) for k in ('host_url', 'secret_token')))
+        return all((self.get_option(k, project)
+                    for k in ('host_url', 'secret_token')))
 
     def get_new_issue_title(self, **kwargs):
         return 'Create Fogbugz case'
@@ -68,6 +71,13 @@ class FogbugzPlugin(IssuePlugin):
         bug.sTitle = form_data['title'].encode('utf8')
         bug.sEvent = form_data['description'].encode('utf8')
         bug.sFormat = u'html'.encode('utf8')
+
+        if self.get_option('area', group.project):
+            bug.sArea = self.get_option('area', group.project)
+        if self.get_option('category', group.project):
+            bug.sCategory = self.get_option('category', group.project)
+        if self.get_option('project', group.project):
+            bug.sProject = self.get_option('project', group.project)
 
         ixBug = fb.new(bug, bugtype={})
         return ixBug.numerator
